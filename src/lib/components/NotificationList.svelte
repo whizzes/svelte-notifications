6bdc7c5a070982cb0faceca0b7f389e05e4d53ad
Michael Liendo <michael.m.liendo.r@gmail.com>
2024-01-06 22:15:53 -0400

    feat: use CSS instead of TailwindCSS (#82)

--- src/lib/components/NotificationList.svelte
+++ src/lib/components/NotificationList.svelte
@@ -9,15 +9,15 @@
   export function getPositionClasses(position: Position = Position.TopRight) {
     switch (position) {
       case Position.TopRight:
-        return 'top-6 right-6';
+        return 'position-top-right';
       case Position.TopLeft:
-        return 'top-6 left-6';
+        return 'position-top-left';
       case Position.BottomRight:
-        return 'bottom-6 right-6';
+        return 'position-bottom-right';
       case Position.BottomLeft:
-        return 'bottom-6 left-6';
+        return 'position-bottom-left';
       default:
-        return 'top-6 right-6';
+        return 'position-top-right';
     }
   }
 </script>
@@ -48,13 +48,52 @@
 </script>
 
 <ul
-  class={`fixed p-6 space-y-4 overflow-y-auto max-h-[calc(100vh-54px)] ${className} ${getPositionClasses(
-    position
-  )}`}
-  class:z-10={notificationsList?.length}
+  class={className}
+  class:position-top-right={position === Position.TopRight}
+  class:position-top-left={position === Position.TopLeft}
+  class:position-bottom-right={position === Position.BottomRight}
+  class:position-bottom-left={position === Position.BottomLeft}
+  class:z-index={notificationsList?.length}
   {style}
 >
   {#each notificationsList as notification}
     <slot {notification} />
   {/each}
 </ul>
+
+<style>
+  .notification-list {
+    position: fixed;
+    padding: 6px;
+    overflow-y: auto;
+    max-height: calc(100vh - 54px);
+  }
+
+  .z-index {
+    z-index: 50;
+  }
+
+  .position-top-right {
+    position: absolute;
+    top: 6px;
+    right: 6px;
+  }
+
+  .position-top-left {
+    position: absolute;
+    top: 6px;
+    left: 6px;
+  }
+
+  .position-bottom-right {
+    position: absolute;
+    bottom: 6px;
+    right: 6px;
+  }
+
+  .position-bottom-left {
+    position: absolute;
+    bottom: 6px;
+    left: 6px;
+  }
+</style>
